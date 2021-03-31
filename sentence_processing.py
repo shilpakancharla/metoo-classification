ce9d17a37009b73faf5e53131dea9f685f675c35
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-03-31 14:16:28 -0400

    Added functions for sentence processing

---
+++ sentence_processing.py
@@ -0,0 +1,31 @@
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+l = nltk.stem.WordNetLemmatizer()
+
+def parts_of_speech_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+def get_lemma(sentence):
+    # Find all parts of speech for each token
+    tagged = nltk.pos_tag(nltk.word_tokenize(sentence))
+    word_net_tagged = map(lambda x: (x[0], posTag(x[1])), tagged)
+    lemmatized_sentence = []
+    for word, tag in word_net_tagged:
+        if word == 'ass':
+            lemmatized_sentence.append(word)
+        elif tag is None:
+            lemmatized_sentence.append(word)
+        else:
+            lemmatized_sentence.append(l.lemmatize(word, tag))
+    return " ".join(lemmatized_sentence)
